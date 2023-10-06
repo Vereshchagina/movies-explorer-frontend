9e5cba58ed3554855aa932a5d0a1a5067296cbb4
Elena Vereshchagina <Elen.Vereshchagina@gmail.com>
2023-10-06 22:11:15 +0300

    Add code for Profile mobile and Preloader style

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,6 +1,19 @@
 import "./Movies.css";
 
+import Header from "../Header/Header";
+import MovieCard from "../MoviesCard/MoviesCard";
+
 function Movies() {
+    return (
+        <>
+            <Header isLogged={true} />
+            <main>
+                <MovieCard />
+            </main>
+        </>
+
+    )
+
 
 }
 
